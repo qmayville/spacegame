951f02f5796439eb0df72badbb2b3255e267b09d
vuem <vwjm13@gmail.com>
2017-03-05 13:35:26 -0600

    added new pngs to gamescreen

--- src/gameWindow.java
+++ src/gameWindow.java
@@ -4,10 +4,9 @@ import javafx.scene.Scene;
 import javafx.scene.canvas.Canvas;
 import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.image.Image;
-import javafx.scene.paint.Color;
-import javafx.scene.text.Font;
-import javafx.scene.text.FontWeight;
 import javafx.stage.Stage;
+
+import java.util.Random;
 
 /**
  * Created by envy on 3/4/17.
@@ -17,10 +16,27 @@ public class gameWindow extends Application{
 //    private static Stage gameStage;
     static Stage gameStage = new Stage();
     private Scene gameScene;
+
+    public Image ship;
     private int shipWidth = 60;
     private int shipHeight = 80;
     public int shipYCoord;
+//    variable kept as private because the x coord for the ship should never change
     private int shipXCoord = 250;
+
+//    Items here should be kept public because we're going to have to modify it later
+    public Image fuel;
+    private int fuelWidth = 40;
+    private int fuelHeight = 60;
+
+    public Image asteroid;
+    public int asteroidSize;
+//    public int asteroidHeight;
+
+    public Image fuelGauge;
+    private int fuelGaugeHeight = 300;
+    private int fuelGaugeWidth = 1000;
+
 
     @Override
     public void start(Stage primaryStage) throws Exception {
@@ -49,11 +65,28 @@ public class gameWindow extends Application{
 
         GraphicsContext graphics = canvas.getGraphicsContext2D();
 
-        Image ship = new Image( "ship.png", shipWidth, shipHeight, true, true );
+        ship = new Image("resources/ship.png", shipWidth, shipHeight, true, true );
+        fuel = new Image("resources/fuel.png", fuelWidth, fuelHeight, true, true);
+
+        asteroidSize = randomAsteroidSize();
+
+        asteroid = new Image("resources/asteroid.png", asteroidSize, asteroidSize, true, true);
+        fuelGauge = new Image("resources/bar.png", fuelGaugeWidth, fuelGaugeHeight, true, true);
 
         shipYCoord = 600;
 
-        graphics.drawImage( ship, shipXCoord, shipYCoord );
+        graphics.drawImage(ship, shipXCoord, shipYCoord );
+//        the x and y placement of these units are only temporary
+        graphics.drawImage(fuel, 250, 250);
+        graphics.drawImage(asteroid, 100, 100);
+        graphics.drawImage(fuelGauge,530, 400);
+    }
+
+    public int randomAsteroidSize(){
+        Random rand = new Random();
+        int randomSize = rand.nextInt(100);
+
+        return randomSize;
     }
 
 }
