86f0ff244e58e740cb6c421237dca9679b01bc3e
mayvilleq <mayvilleq@cmc304-02.mathcs.carleton.edu>
2017-03-08 18:12:20 -0600

    Fixed fuel indicator somewhat

---
+++ src/FuelIndicatorSprite.java
@@ -0,0 +1,32 @@
+import javafx.scene.image.Image;
+
+/**
+ * Created by mayvilleq on 3/8/17.
+ */
+public class FuelIndicatorSprite extends AbstractSprite {
+    private static final double MIN_Y = 400;
+
+    public FuelIndicatorSprite(double PositionX, double PositionY, Image image,double velocityY) {
+        super(PositionX, PositionY, image);
+        setVelocityY(velocityY);
+    }
+
+    private void boundY() {
+        if (getPositionY() < MIN_Y) {
+            setPositionY(MIN_Y);
+        }
+    }
+
+    @Override
+    public void setPositionY(double y) {
+        super.setPositionY(y);
+        boundY();
+    }
+
+    @Override
+    public void updatePositionY(double time) {
+        super.updatePositionY(time);
+        boundY();
+    }
+
+}
